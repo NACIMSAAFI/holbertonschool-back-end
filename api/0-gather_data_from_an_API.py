8d2da46dc3dffe74eb8cf99181630cdcd39e7312
NACIMSAAFI <7715@holbertonstudents.com>
2024-04-26 16:26:46 +0100

    Change code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -17,50 +17,25 @@ def fetch_todo_list_progress(employee_id):
     Returns:
         None
     """
-    # Base URL for the fake REST API
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    # URL to fetch user information
-    user_url = f"{base_url}/users/{employee_id}"
-    # URL to fetch TODO list for the user
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    # Fetch user information
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    # Extract employee name from user data
-    employee_name = user_data.get("name")
-
-    # Fetch TODO list for the user
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    # Calculate progress
-    tasks = len(todos_data)
-    # Count number of completed tasks
-    task = sum(True for todo in todos_data if todo["completed"])
-
-    # Display progress
-    print(f"Employee {employee_name} is done with tasks ({task}/{tasks}):")
-    # Display titles of completed tasks
-    for todo in todos_data:
-        if todo["completed"]:
-            print(f"\t{todo['title']}")
+    todos = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+        ).json()
+    user_info = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+        ).json()
+    completed_tasks = [task["title"] for task in todos if task["completed"]]
+    a = len(completed_tasks)
+    b = len(todos)
+    print(
+        f"Employee {user_info['name']} is done with tasks ({a}/{b}):")
+    for task in completed_tasks:
+        print("\t{}".format(task))
 
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
+    if len(sys.argv) < 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    # Extract employee ID from command line argument
     employee_id = sys.argv[1]
-    try:
-        employee_id = int(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1)
-
-    # Call function to fetch and display TODO list progress
     fetch_todo_list_progress(employee_id)
